d8191ccb281ba33804e3526e352af198014e3d9b
Magmagan <alexlalves@live.com>
2020-05-06 03:33:14 -0300

    create ShowColors spec

---
+++ tests/components/ShowColors.spec.ts
@@ -0,0 +1,42 @@
+import { shallowMount } from '@vue/test-utils';
+import ShowColors from '@/components/ShowColors.vue';
+
+describe('InitialInfo', () => {
+  def('colors', () => ['white', 'yellow']);
+
+  const wrapper = () => shallowMount(
+    ShowColors, {
+      propsData: {
+        colors: get('colors'),
+    },
+  });
+
+  it('matches snapshot', () => {
+    expect(wrapper().element).toMatchSnapshot();
+  });
+
+  describe('numberOfColumns', () => {
+    describe('when the number of colors is a perfect square', () => {
+      def('colors', () => [
+        'white', 'yellow',
+        'blue', 'green',
+      ]);
+
+      it('retuns the exact amount of columns needed', () => {
+        expect(wrapper().vm.numberOfColumns).toEqual(2);
+      });
+    });
+
+    describe('when the number of colors is bigger than a perfect square', () => {
+      def('colors', () => [
+        'white', 'yellow',
+        'blue', 'green',
+        'orange',
+      ]);
+
+      it('retuns an extra column', () => {
+        expect(wrapper().vm.numberOfColumns).toEqual(3);
+      });
+    });
+  });
+});
