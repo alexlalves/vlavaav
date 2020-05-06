bbb6e26bd5a4b2017a77fc9e1e8e76f41738943c
Magmagan <alexlalves@live.com>
2020-05-06 01:45:18 -0300

    fill ShowColors with css grid & styles and copy function

--- src/components/ShowColors.vue
+++ src/components/ShowColors.vue
@@ -1,14 +1,26 @@
 <template>
-  <div class="hello">
+  <div>
     <div
-      v-for="color in colors"
-      :key="color"
-      :style="`background-color: ${color}; flex-grow: 1`"
-      class="color"
+      class="show-colors"
+      :style="`grid-template-columns: repeat(${numberOfColumns}, 1fr)`"
     >
-      <p>
-        {{ color }}
-      </p>
+      <div
+        v-for="color in colors"
+        :key="color"
+        :style="`background-color: ${color}; flex-grow: 1`"
+        @click="copyToClipboard(color)"
+        class="show-colors__cell"
+      >
+        <div class="show-colors__cell-description">
+          {{ color }}
+          <br>
+        </div>
+      </div>
+    </div>
+    <div class="show-colors__footer">
+      <a href="." class="show-colors__footer-link">Click here</a>
+      to paste a new set of colors.
+      <strong>VLAVAAV</strong> © Alex L. Alves 2020.
     </div>
   </div>
 </template>
@@ -28,14 +40,49 @@ export default class ShowColors extends Vue {
 
     return isSquare ? squareRoot : squareRoot + 1;
   }
+
+  public copyToClipboard(color: string) {
+    navigator.clipboard.writeText(color);
+  }
 }
 </script>
 
 <style scoped lang="scss">
-.hello {
-  height: 100%;
+.show-colors {
+  display: grid;
+  height: calc(100vh - 2em);
+  width: 100vw;
+}
+
+.show-colors__cell {
+  align-items: flex-end;
+  cursor: pointer;
   display: flex;
-  flex-direction: column;
+  font-size: larger;
+  justify-content: center;
+  overflow: hidden;
+
+  &:hover  ::after {
+    content: '(click to copy)';
+  }
+}
+
+.show-colors__cell-description {
+  background-color: rgba(12, 12, 12, 0.8);
+  padding: .5em;
   width: 100%;
+}
+
+.show-colors__footer {
+  height: 2em;
+  line-height: 2em;
+}
+
+.show-colors__footer-link {
+  color: #ffffff;
+
+  &:hover {
+    text-decoration: none;
+  }
 }
 </style>
