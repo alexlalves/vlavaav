91b2684c76f1a8c1ccdf21a5113c7b9052fd0c6c
Alex L. Alves <alexlalves@live.com>
2021-08-12 20:50:32 -0300

    add /palette route for results page

---
+++ src/views/Palette.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="palette">
+    Hi! {{ colors }}
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class App extends Vue {
+  @Prop({ type: String, default: '' }) readonly colors!: string
+}
+</script>
