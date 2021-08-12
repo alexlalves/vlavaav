545da4b62e0c3e4ada5e4aaebd0489668edb7602
Alex L. Alves <alexlalves@live.com>
2021-08-12 14:47:19 -0300

    show initial info in about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <InitialInfo/>
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import InitialInfo from '@/components/InitialInfo.vue';
+
+@Component({
+  components: {
+    InitialInfo,
+  },
+})
+export default class App extends Vue { }
+</script>
