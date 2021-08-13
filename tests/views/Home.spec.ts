dcb8d89885a7e23f45156fb7a5180566fc69883d
Alex L. Alves <alexlalves@live.com>
2021-08-13 00:22:33 -0300

    fix tests for Home view

--- tests/views/Home.spec.ts
+++ tests/views/Home.spec.ts
@@ -1,45 +1,44 @@
 import { shallowMount } from '@vue/test-utils';
-import App from '@/App.vue';
+import Home from '@/views/Home.vue';
 
-describe('App', () => {
-  def('wrapper', () => shallowMount(App));
-  const wrapper = shallowMount(App);
+describe('Home', () => {
+  const wrapper = () => shallowMount(Home);
 
   it('matches snapshot', () => {
-    expect(wrapper.element).toMatchSnapshot();
+    expect(wrapper().element).toMatchSnapshot();
   });
 
   describe('findColors', () => {
     describe('when text with no color is passed', () => {
       it('returns an empty list', () => {
         expect(
-          wrapper.vm.findColors('Color: white :('),
-        ).toEqual([]);
+          wrapper().vm.findColors('Color: white :('),
+        ).toEqual('');
       });
     });
 
     describe('when an uppercase color is passed', () => {
       it('returns that color', () => {
         expect(
-          wrapper.vm.findColors('Color: #FF00FF'),
-        ).toContain('#ff00ff');
+          wrapper().vm.findColors('Color: #FF00FF'),
+        ).toMatch('ff00ff');
       });
     });
 
     describe('when more than one color is passed', () => {
       describe('when they are unique', () => {
         it('returns those colors', () => {
-          const colors = wrapper.vm.findColors('ColorA: #FF00FF #abcde0');
+          const colors = wrapper().vm.findColors('ColorA: #FF00FF #abcde0');
 
-          expect(colors).toContain('#ff00ff');
-          expect(colors).toContain('#abcde0');
+          expect(colors).toMatch('ff00ff');
+          expect(colors).toMatch('abcde0');
         });
       });
 
       describe('when they are duplicated', () => {
         it('returns those colors', () => {
-          const colors = wrapper.vm.findColors('ColorA: #FF00FF #ff00ff');
-          expect(colors).toEqual(['#ff00ff']);
+          const colors = wrapper().vm.findColors('ColorA: #FF00FF #ff00ff');
+          expect(colors).toEqual('ff00ff');
         });
       });
     });
