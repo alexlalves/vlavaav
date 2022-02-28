f5e114eb4cfe19c67aff2dc5a16cad7fedef6c58
Alex L. Alves <alexlalves@live.com>
2022-02-28 16:59:52 -0300

    create and update typescript definition files

--- src/components/InitialInfo.vue.d.ts
+++ src/components/InitialInfo.vue.d.ts
@@ -1,5 +1,3 @@
 import { Vue } from 'vue-property-decorator';
 
-export default class InitialInfo extends Vue {
-    private sample_palette;
-}
+export default class InitialInfo extends Vue { }
--- src/views/Home.vue.d.ts
+++ src/views/Home.vue.d.ts
@@ -1,13 +1,3 @@
 import { Vue } from 'vue-property-decorator';
 
-export default class Home extends Vue {
-  private colors;
-
-  private errorMessageToShow;
-
-  onAppPaste(pastedContent: ClipboardEvent): void;
-
-  findColors(pastedText: string): string;
-
-  readonly showInitialInfo: boolean;
-}
+export default class Home extends Vue { }
