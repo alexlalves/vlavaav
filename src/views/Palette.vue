8e9036a616dd796cbdb1d26ed0c40440d275a613
Alex L. Alves <alexlalves@live.com>
2022-03-02 19:52:06 -0300

    move out footer from ShowColors and add small header

--- src/views/Palette.vue
+++ src/views/Palette.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="palette">
-    <ShowColors :commaSeparatedColors="colors"/>
+    <div class="palette__header">
+      <span class="palette__hide-mobile">Click</span>
+      <span class="palette__show-mobile">Tap</span>
+      a color to copy it to your clipboard.
+    </div>
+
+    <div class="palette__colors-wrapper">
+      <ShowColors :commaSeparatedColors="colors"/>
+    </div>
+
+    <div class="palette__footer">
+      <router-link :to="{name: 'Home'}" class="palette__footer-link">Click here</router-link>
+      to paste a new set of colors.
+      <strong>VLAVAAV</strong> © Alex L. Alves 2021.
+    </div>
   </div>
 </template>
 
@@ -17,3 +31,52 @@ export default class Palette extends Vue {
   @Prop({ type: String, default: '' }) readonly colors!: string
 }
 </script>
+
+<style scoped lang="scss">
+.palette {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  width: 100%;
+}
+
+.palette__show-mobile {
+  @media (min-width: 480px) {
+    display: none;
+  }
+}
+
+.palette__hide-mobile {
+  display: none;
+
+  @media (min-width: 480px) {
+    display: unset;
+  }
+}
+
+.palette__colors-wrapper {
+  margin: 16px;
+}
+
+.palette__header {
+  background-color: #222;
+  height: 2em;
+  line-height: 2em;
+}
+
+.palette__footer {
+  margin-top: auto;
+  min-height: 2em;
+  line-height: 2em;
+  background-color: #222;
+  color: #DDD;
+}
+
+.palette__footer-link {
+  color: #ffffff;
+
+  &:hover {
+    text-decoration: none;
+  }
+}
+</style>
