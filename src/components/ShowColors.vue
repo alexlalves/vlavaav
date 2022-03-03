748732c5fbafc50ccbaabb3c51e9d0f9e2c59358
Alex L. Alves <alexlalves@live.com>
2022-03-02 21:33:09 -0300

    use dynamic grid sizing on desktop, 2 columns mobile

--- src/components/ShowColors.vue
+++ src/components/ShowColors.vue
@@ -1,21 +1,15 @@
 <template>
   <div>
-    <div
-      class="show-colors"
-      :style="`grid-template-columns: repeat(${numberOfColumns}, 1fr)`"
-    >
+    <div class="show-colors">
       <div
         v-for="color in colors"
         :key="color"
-        :style="`background-color: #${color}; flex-grow: 1`"
+        :style="`background-color: #${color};`"
         @click="copyToClipboard(color)"
         class="show-colors__cell"
       >
         <div class="show-colors__cell-description-box">
-          <code class="show-colors__cell-description">
-            {{ color }}
-            <br>
-          </code>
+          <code>{{ color }}</code>
         </div>
       </div>
 
@@ -24,17 +18,9 @@
         class="show-colors__cell"
       >
         <div class="show-colors__cell-description-box">
-          <code class="show-colors__cell-description">
-            Share this palette
-            <br>
-          </code>
+          <code>Share this palette</code>
         </div>
       </div>
-    </div>
-    <div class="show-colors__footer">
-      <router-link :to="{name: 'Home'}" class="show-colors__footer-link">Click here</router-link>
-      to paste a new set of colors.
-      <strong>VLAVAAV</strong> © Alex L. Alves 2021.
     </div>
   </div>
 </template>
@@ -74,42 +60,29 @@ export default class ShowColors extends Vue {
 <style scoped lang="scss">
 .show-colors {
   display: grid;
-  height: calc(100vh - 2em);
-  width: 100vw;
+  grid-gap: 16px;
+  grid-template-columns: 1fr 1fr;
+
+  @media (min-width: 480px) {
+    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  }
 }
 
 .show-colors__cell {
   align-items: flex-end;
   cursor: pointer;
   display: flex;
-  justify-content: center;
+  height: 160px;
 
-  &:hover .show-colors__cell-description:after {
-    content: '(click to copy)';
+  &:hover {
+    transform: scale(1.05);
   }
 }
 
 .show-colors__cell-description-box {
   background-color: rgba(12, 12, 12, 0.8);
-  font-size: larger;
+  font-size: 1.25em;
   padding: .5em;
   width: 100%;
-}
-
-.show-colors__cell-description {
-  font-size: larger;
-}
-
-.show-colors__footer {
-  height: 2em;
-  line-height: 2em;
-}
-
-.show-colors__footer-link {
-  color: #ffffff;
-
-  &:hover {
-    text-decoration: none;
-  }
 }
 </style>
