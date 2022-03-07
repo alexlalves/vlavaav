93377960ed427a923f0ca97104da26f939b77a67
Alex L. Alves <alexlalves@live.com>
2022-03-07 17:34:47 -0300

    add sample palettes

---
+++ src/assets/sample_palettes/index.ts
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/camelcase */
+export default {
+  VLAVAAV: 'vlavaav',
+  CGA: 'cga',
+  COMMODORE: 'commodore64',
+  WEBSAFE: 'websafe_colors',
+  ZX_SPECTRUM: 'zx_spectrum',
+};
