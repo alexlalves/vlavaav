886cca5e437a5da68ed7f69328f4828aa295d97c
Alex L. Alves <alexlalves@live.com>
2022-03-07 17:56:20 -0300

    add v-button amd adapt buttons to new palette routing

--- src/components/ColorsForm.vue
+++ src/components/ColorsForm.vue
@@ -1,32 +1,51 @@
 <template>
-  <form @submit.prevent="formSubmit">
+  <form @submit.prevent>
     <p v-if="errorMessageToShow">
       {{ errorMessageToShow }}
     </p>
 
     <textarea
       class="colors-form__input-area"
-      :placeholder=samplePalette
+      :placeholder=exampleText
       v-model="pastedInfo"
     />
 
-    <button
-      class="colors-form__submit-button"
-      type="submit"
-    >
-      Generate
-    </button>
+    <div class="colors-form__button-container">
+      <v-button
+        class="colors-form__submit-button"
+        type="submit"
+        value="generate"
+        @click="submitGenerate"
+      >
+        Generate
+      </v-button>
+
+      <v-button
+        class="colors-form__submit-button colors-form__submit-button--random"
+        type="submit"
+        value="random"
+        @click="submitRandom"
+      >
+        Surprise me!
+      </v-button>
+    </div>
   </form>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import SAMPLE_PALETTES from '@/assets/sample_palettes';
+import VButton from '@/components/VButton.vue';
 
-@Component
+@Component({
+  components: {
+    VButton,
+  },
+})
 export default class ColorsForm extends Vue {
-  private samplePalette = [
-    'Paste here or just click \'Generate\'',
-    'and see these colors in action:',
+  private exampleText = [
+    'Paste here your colors and click \'Generate\'.',
+    'Example input:',
     '',
     'V: #ff0000,',
     'L: #ff8800,',
@@ -41,14 +60,43 @@ export default class ColorsForm extends Vue {
 
   private pastedInfo = '';
 
-  public formSubmit() {
-    const colors = this.findColors(this.pastedInfo || this.samplePalette);
+  public submitGenerate() {
+    const {
+      pastedInfo,
+      $router,
+      findColors
+    } = this;
 
-    if (colors.length) {
-      this.$router.push({ name: 'Palette', params: { colors } });
+    if (!pastedInfo) {
+      $router.push({
+        name: `Palette-${SAMPLE_PALETTES.VLAVAAV}`,
+      });
+    }
+
+    const colors = findColors(pastedInfo);
+
+    if(colors.length) {
+      $router.push({
+        name: 'Palette',
+        query: { colors },
+      });
     } else {
       this.errorMessageToShow = 'No colors of the #RRGGBB format were found in the submitted text.';
     }
+  }
+
+  public submitRandom() {
+    const allPalettes = Object.values(SAMPLE_PALETTES);
+
+    const randomPalette = allPalettes[
+      Math.floor(
+        allPalettes.length * Math.random()
+      )
+    ];
+
+    this.$router.push({
+      name: `Palette-${randomPalette}`,
+    });
   }
 
   public findColors(pastedText: string): string {
@@ -64,6 +112,8 @@ export default class ColorsForm extends Vue {
 <style scoped lang="scss">
 .colors-form__input-area {
   background-color: var(--textarea-color);
+  border: 0;
+  border-radius: 8px;
   box-sizing: border-box;
   color: var(--text-color);
   display: block;
@@ -81,20 +131,25 @@ export default class ColorsForm extends Vue {
   }
 }
 
+.colors-form__button-container {
+  display: grid;
+  grid-template-columns: 2fr 2fr;
+  grid-gap: 16px;
+
+  @media (min-width: 480px) {
+    grid-gap: 24px;
+    grid-template-columns: 3fr 2fr;
+  }
+}
+
 .colors-form__submit-button {
-  background-color: var(--button-color);
-  border: none;
-  box-sizing: border-box;
-  color: var(--text-color);
   cursor: pointer;
   font-size: 1.1em;
   margin-bottom: 0;
   padding: 1em;
-  transition: 0.2s ease-in-out;
-  width: 100%;
 
-  &:hover {
-    background-color: var(--button-highlighted-color);
+  &--random {
+    font-size: unset;
   }
 }
 </style>
