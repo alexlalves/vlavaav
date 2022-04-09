64aff23043113a815dc170972514de0959533dfb
Alex L. Alves <alexlalves@live.com>
2022-04-09 20:54:11 -0300

    spill dist/ to project root

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
-  testMatch: [
-    '<rootDir>/tests/**/*.spec.(js|jsx|ts|tsx)',
-  ],
-  setupFilesAfterEnv: [
-    '<rootDir>/tests/jest-after-env.ts',
-  ],
-};
--- src/components/ShowColors.vue.d.ts
+++
@@ -1,9 +0,0 @@
-import { Vue } from 'vue-property-decorator';
-
-export default class ShowColors extends Vue {
-  readonly colors: string[];
-
-  readonly numberOfColumns: number;
-
-  copyToClipboard(color: string): void;
-}
